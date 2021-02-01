23e5da48610b41bbed8c554c01bcb1978add0a57
ParamagicDev <konnor5456@gmail.com>
2021-02-01 11:18:09 -0500

    it works!

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'fileutils'
-require "rake"
+require 'json'
+require 'yaml'
 
 DISCLAIMER = <<~CAUTION
   WARNING:
@@ -24,11 +25,7 @@ def main
     return
   end
 
-  say 'Installing Snowpack', :green
-  install_snowpack
-
-  say 'Adding Snowpack configs', :green
-  add_snowpack_files
+  @package_json = JSON.parse(File.read("package.json"))
 
   say 'Removing Webpack', :blue
   FileUtils.rm 'webpack.config.js' if File.exist?("webpack.config.js")
@@ -38,10 +35,43 @@ def main
 
   say 'Removing unneeded npm packages', :blue
   remove_npm_packages
+
+  say 'Adding Snowpack configs', :green
+  add_snowpack_files
+
+  say 'Adding Snowpack scripts', :green
+  add_snowpack_scripts
+
+  File.write("package.json", JSON.pretty_generate(@package_json))
+
+  say "Rewriting frontend/javascript/index.js"
+  rewrite_index_js
+
+  say "Rewriting webpack references in src/_components/head.liquid"
+  rewrite_webpack_references
+
+  say "Updating your bridgetown config..."
+  update_bridgetown_config
+
+  say 'Installing Snowpack', :green
+  install_snowpack
+
+  say "Installation successful! 🎉", :green
+
+  say "\n\nRun `yarn start` to start up your dev server and go to `localhost:4000`"
 end
 
 def install_snowpack
-  run('yarn add snowpack @snowpack/plugin-run-script')
+  run('yarn add snowpack @snowpack/plugin-run-script @snowpack/plugin-sass')
+end
+
+def rewrite_index_js
+  index_js = <<~JAVASCRIPT
+    import "../styles/index.scss"
+    console.info("Bridgetown is loaded!")
+  JAVASCRIPT
+
+  create_file("frontend/javascript/index.js", index_js)
 end
 
 def add_snowpack_files
@@ -53,26 +83,28 @@ def add_snowpack_files
     module.exports = {
       mount: {
         frontend: "/dist",
-        output: { url: "/", static: true },
-        public: { url: "/", static: true, resolve: false },
+        ".bridgetown": { url: "/", static: true },
+        output: { url: "/", static: true, resolve: false },
       },
       plugins: [
         [
           "@snowpack/plugin-run-script",
           {
             name: "bridgetown",
-            cmd: "bin/bridgetown build",
+            cmd: "bundle exec bridgetown build",
             watch: "$1 --watch --quiet",
           },
         ],
+        ["@snowpack/plugin-sass", {compilerOptions: { style: "compressed" }}],
       ],
       devOptions: {
         port: 4000,
         hmrDelay: 1000,
         open: "none",
+        output: "stream"
       },
       buildOptions: {
-        metaUrlPath: "output/dist/javascript",
+        metaUrlPath: "dist/javascript",
         out: "output"
       },
       optimize: {
@@ -113,10 +145,38 @@ def remove_npm_packages
 
   npm_packages.each do |pkg|
     say "Removing: #{pkg}..."
-    gsub_file("package.json", %r("#{pkg}": ".*",?[\n\r]), "")
+    @package_json["devDependencies"].delete(pkg)
   end
+end
 
-  run "yarn install"
+def add_snowpack_scripts
+  scripts = {
+    "build:bridgetown" => "bin/bridgetown build",
+    "build" => "NODE_ENV=production BRIDGETOWN_ENV=production snowpack build",
+    "clean:bridgetown" => "bundle exec bridgetown clean",
+    "restart" => "yarn clean:bridgetown && yarn start --reload",
+    "start" => "NODE_ENV=development BRIDGETOWN_ENV=development snowpack dev",
+    "deploy" => "yarn clean:bridgetown && yarn build"
+  }
+  @package_json["scripts"].merge!(scripts)
+end
+
+def rewrite_webpack_references
+  head_liquid = "src/_components/head.liquid"
+  gsub_file(head_liquid, %r!{%\s+webpack_path js\s+%}!, "/dist/javascript/index.js")
+  gsub_file(head_liquid, %r!defer!, "type='module' defer")
+  gsub_file(head_liquid, %r!{%\s+webpack_path css\s+%}!, "/dist/styles/index.css")
+end
+
+def update_bridgetown_config
+  config_file = "bridgetown.config.yml"
+  config = YAML.load(File.read(config_file))
+
+  config["development"] = { "url" => "http://localhost:4000" }
+  config["destination"] = ".bridgetown"
+
+  File.write(config_file, config.to_yaml)
+  gsub_file(config_file, /---\n/, "")
 end
 
 main
